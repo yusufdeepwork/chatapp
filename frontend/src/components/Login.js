551bc26211014eb9245c96d2293822bd1efc7bd9
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-06-13 22:32:03 +0300

    change & update Login component responsively & logically

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,29 +1,63 @@
 import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
+import { ToastContainer, toast } from 'react-toastify';
 import { MessagesContext } from '../context/MessagesContext';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
-  const { setLiveUser, setUsers } = useContext(MessagesContext);
-  const [newUserName, setNewUserName] = useState('');
-  const createUser = () => {
-    // eslint-disable-next-line max-len
-    setUsers((prevUsers) => [...prevUsers, { userName: newUserName, id: (Math.random() * 100).toFixed(0) }]);
+  const { setLiveUser, setUsers, liveUser } = useContext(MessagesContext);
+  const [newUserName, setNewUserName] = useState();
+  const handleEnter = (event) => (event.key === 'Enter' ? addLiveUser() : null);
+
+  const createLoginUser = () => {
+    setUsers((prevUsers) => [...prevUsers, { userName: liveUser.loginUserId }]);
+  };
+  const addLiveUser = () => {
+    if (newUserName) {
+      setLiveUser((prevState) => ({
+        ...prevState,
+        loginUserName: newUserName,
+        isLogin: true,
+        loginUserId: (Math.random() * 100).toFixed(0),
+      }));
+      createLoginUser();
+    } else {
+      toast.error('Please, type a name!', {
+        position: 'top-right',
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
   };
 
   return (
     <LoginContainer>
-      <input
+      <LoginInput
         placeholder="enter your name"
+        value={newUserName}
+        onKeyDown={handleEnter}
         onChange={((event) => setNewUserName(event.target.value))}
       />
+
       {/* eslint-disable-next-line react/button-has-type */}
-      <button onClick={() => {
-        setLiveUser((prevState) => ({ ...prevState, isLogin: true }));
-        createUser();
-      }}
-      >
-        Login & Enjoy Talking
-      </button>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <LoginButton onClick={() => addLiveUser()}>
+        Login
+      </LoginButton>
     </LoginContainer>
   );
 };
@@ -35,4 +69,56 @@ const LoginContainer = styled.div`
   align-items: center;
   height: 100%;
   width: 70%;
+  font-family:  'Quicksand', sans-serif;
+`;
+const LoginInput = styled.input`
+  font-family: inherit;
+  width: 60%;
+  padding: 1rem;
+  font-size: 30px;
+  border: none;
+  margin: 1rem;
+  outline: inherit;
+  :focus{
+    border:#bbb6c6 solid 4px;
+  }
+  @media screen and (max-width: 800px){
+    font-size: 20px;
+  }
+  @media screen and (max-width: 550px){
+    width: 80%;
+    font-size: 15px;
+  }
+`;
+const LoginButton = styled.button`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: inherit;
+  height: 3rem;
+  width: 15rem;
+  border-radius: 1rem;
+  font-size: 20px;
+ 
+  margin: 1rem;
+  background-color: royalblue;
+  border: none;
+  color: lightcyan;
+  :hover{
+    cursor: pointer;
+    background-color: deepskyblue;
+
+  }
+  @media screen and (max-width: 800px){
+    font-size: 15px;
+    width: 12rem;
+    height: 2rem;
+    padding: 0.5rem;
+    margin: 0.5rem;
+  }
+  @media screen and (max-width: 550px){
+    width: 40%;
+    font-size: 15px;
+  }
 `;
