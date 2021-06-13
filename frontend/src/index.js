856d953cff81d68a09770abec535ca22bf06c34f
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-06-13 19:02:13 +0300

    add Chat Provider to in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ChatAppProvider from './context/MessagesContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChatAppProvider>
+      <App />
+    </ChatAppProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
