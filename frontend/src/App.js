6c04483aa18e318045294439f1db798d73d00ecd
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-06-13 13:26:31 +0300

    add frontend initially

---
+++ frontend/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import styled from 'styled-components';
+import { ChatsBox, MessagingBox } from './components';
+
+const socket = io.connect('http://localhost:5555');
+
+function App() {
+  const [message, setMessage] = useState({ context: '', name: '' });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on('message', ({ context, name }) => {
+      setChat([...chat, { context, name }]);
+    });
+  }, [chat]);
+
+  const onTextChange = (e) => {
+    setMessage({ ...message, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { context, name } = message;
+    socket.emit('message', { context, name });
+    setMessage({ context: '', name: '' });
+  };
+
+  return (
+    <ChatAppBody>
+      <ChatAppContainer>
+        <ChatsBox />
+        <MessagingBox />
+      </ChatAppContainer>
+    </ChatAppBody>
+  );
+}
+
+export default App;
+const ChatAppBody = styled.div`
+  display: flex;
+  height:100vh;
+  width: 100%;
+  background-color: #c2bcce;
+  justify-content: center;
+  align-items: center;
+;
+`;
+const ChatAppContainer = styled.div`
+  display: flex;
+  height: 70%;
+  width: 85%;
+  border-radius: 2rem;
+  background-color: #f2f1f4;
+  justify-content: center;
+  align-items: center;
+`;
